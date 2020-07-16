49e6656ec2b282ac89ec7e07eedb3dfaac080f0a
Jorge Alfredo Alvarez Guzman <jorgealfredoalvarezguzman@Jorges-MacBook-Pro.local>
2020-07-15 20:43:04 -0500

    Add Entry Page requirement

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -10,7 +12,14 @@ def index(request):
 
 
 def title(request, title):
+    entry = util.get_entry(title)
+
+    if entry is None:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+
     return render(request, "encyclopedia/title.html",{
         "title": title,
-        "entry": util.get_entry(title)
+        "entry": markdown2.markdown(entry)
     })
